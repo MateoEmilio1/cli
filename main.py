0d13474cc461dffa9df81b2bdcc2dd8f40007303
MateoEmilio1 <mateoyastor60@gmail.com>
2025-02-08 12:00:55 -0300

    refactor: read folder + stakers

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from networks import router as networks_router
-from operators import router as operators_router
-from vaults import router as vaults_router
+from read_commands.networks import router as networks_router
+from read_commands.operators import router as operators_router
+from read_commands.vaults import router as vaults_router
+from read_commands.stakers import router as stakers_router
 
 app = FastAPI()
 
 app.include_router(networks_router)
 app.include_router(operators_router)
 app.include_router(vaults_router)
+app.include_router(stakers_router)
